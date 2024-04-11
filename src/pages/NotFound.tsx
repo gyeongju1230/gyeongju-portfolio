cad85bbc8104e5162fde3e73b12fd9789e294fa0
zoodaa <jkj01230@gmail.com>
2024-04-11 23:55:59 +0900

    feat: NotFound page 구현

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <p style={{ color: "red" }}>NotFound</p>
+      </header>
+    </div>
+  );
+};
+
+export default NotFound;
