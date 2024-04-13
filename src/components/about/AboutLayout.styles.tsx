b43bc39988ae7b1bb265a18a33c9ff2e3fc20aa2
zoodaa <jkj01230@gmail.com>
2024-04-13 23:49:45 +0900

    feat: About page 구현
    폰트 수정필요

--- src/components/about/AboutLayout.styles.tsx
+++ src/components/about/AboutLayout.styles.tsx
@@ -6,19 +6,113 @@ export const Container = styled.div`
   position: relative;
   width: 100vw;
   height: calc(100vh - 90px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   margin-top: 90px;
   background-color: #ffffff;
+
+  @media (max-width: ${smallViewport}) {
+    height: calc(100vh - 240px);
+    margin-top: 240px;
+
+    &.none-menu {
+      height: calc(100vh - 77px);
+      margin-top: 77px;
+    }
+  }
+`;
+
+export const AboutTitle = styled.div`
+  height: 160px;
+  margin-top: -40px;
+  margin-bottom: 20px;
+  text-align: center;
+  color: #000;
+  font-size: 22px;
+  line-height: 40px;
+  font-family: SUITE, sans-serif;
+
+  @media (max-width: ${smallViewport}) {
+    font-size: 18px;
+    line-height: 30px;
+  }
+`;
+
+export const AboutContainer = styled.div`
+  //width: 791px;
+  width: 60%;
+  height: 382px;
   display: flex;
+  gap: 30px;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 1px 2px 9px rgba(45, 45, 45, 0.3);
+  background-color: #fffbf5;
+
+  @media (max-width: ${smallViewport}) {
+    //width: 395.5px;
+    width: calc(100% - 10%);
+    height: 352px;
+    padding: 10px;
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
+
+export const ImageBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  background-color: #fbe5d8;
+  text-align: center;
+
+  @media (max-width: ${smallViewport}) {
+    width: 100%;
+    height: 45%;
+  }
+`;
+
+export const AboutBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 
   @media (max-width: ${smallViewport}) {
-    margin-top: 240px;
-    height: calc(100vh - 240px);
+    width: 100%;
+    height: 55%;
+  }
+`;
 
-    &.none-menu {
-      margin-top: 77px;
-      height: calc(100vh - 77px);
-    }
+export const AboutContentIcon = styled.div`
+  width: 30px;
+  display: flex;
+  margin-right: 15px;
+  align-items: center;
+`;
+
+export const AboutContent = styled.div`
+  width: 100%;
+  height: 45px;
+  display: flex;
+  margin-bottom: 20px;
+  color: #000;
+  font-size: 19px;
+  line-height: 45px;
+  //font-weight: bold;
+  font-family: Pretendard, sans-serif;
+
+  @media (max-width: ${smallViewport}) {
+    width: 90%;
+    height: 22.5px;
+    margin-bottom: 10px;
+    line-height: 22.5px;
   }
 `;
--- src/components/about/AboutLayout.tsx
+++ src/components/about/AboutLayout.tsx
@@ -1,5 +1,11 @@
-import * as styles from ".//AboutLayout.styles";
+import * as styles from "./AboutLayout.styles";
+import Typewriter from "typewriter-effect";
 import AboutScrollDown from "../common/scroll/scroll-down/ScrollDown";
+import { ReactComponent as Name } from "../../assets/icon/about/NameIcon.svg";
+import { ReactComponent as BirthDate } from "../../assets/icon/about/BirthDateIcon.svg";
+import { ReactComponent as Email } from "../../assets/icon/about/EmailIcon.svg";
+import { ReactComponent as GitHub } from "../../assets/icon/about/GitHubIcon.svg";
+import { ReactComponent as Notion } from "../../assets/icon/about/NotionIcon.svg";
 
 interface AboutLayoutProps {
   isMenuOpen: boolean;
@@ -15,7 +21,65 @@ const AboutLayout = ({ isMenuOpen }: AboutLayoutProps) => {
 
   return (
     <styles.Container id="about" className={isMenuOpen ? "" : "none-menu"}>
-      <h1>About</h1>
+      <styles.AboutTitle>
+        {/*안녕하세요 :)*/}
+        {/*<br />*/}
+        {/*“사용자 중심”의 개발을 추구하며*/}
+        {/*<br />*/}
+        {/*호기심을 통해 혁신적인 솔루션을 찾는*/}
+        {/*<br />*/}
+        {/*프론트엔드 개발자 정경주입니다.*/}
+        <Typewriter
+          options={{
+            strings: [
+              "안녕하세요 :)<br/>“사용자 중심”의 개발을 추구하며<br/>호기심을 통해 혁신적인 솔루션을 찾는<br />프론트엔드 개발자 정경주입니다.",
+            ],
+            autoStart: true,
+            loop: true,
+            delay: 50,
+            deleteSpeed: 50,
+            wrapperClassName: "typewriter",
+          }}
+        />
+      </styles.AboutTitle>
+      <styles.AboutContainer>
+        <styles.ImageBox>
+          📸 <br />
+          프로필 추가 필요
+        </styles.ImageBox>
+        <styles.AboutBox>
+          <styles.AboutContent>
+            <styles.AboutContentIcon>
+              <Name />
+            </styles.AboutContentIcon>
+            정경주
+          </styles.AboutContent>
+          <styles.AboutContent>
+            <styles.AboutContentIcon>
+              <BirthDate />
+            </styles.AboutContentIcon>
+            2000.12.30
+          </styles.AboutContent>
+          <styles.AboutContent>
+            <styles.AboutContentIcon>
+              <Email />
+            </styles.AboutContentIcon>
+            jkj01230@gmail.com
+          </styles.AboutContent>
+          <styles.AboutContent>
+            <styles.AboutContentIcon>
+              <GitHub />
+            </styles.AboutContentIcon>
+            https://github.com/gyeongju1230
+          </styles.AboutContent>
+          <styles.AboutContent>
+            <styles.AboutContentIcon>
+              <Notion />
+            </styles.AboutContentIcon>
+            https://bit.ly/4cUYb0S
+          </styles.AboutContent>
+        </styles.AboutBox>
+      </styles.AboutContainer>
       <AboutScrollDown onClick={scrollToSkill} />
     </styles.Container>
   );
