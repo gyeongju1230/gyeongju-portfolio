d4bd850722f3734fa66839dae5094d745862641b
zoodaa <jkj01230@gmail.com>
2024-04-23 00:28:25 +0900

    feat: Project Content Data 구현
    - Data 파일을 따로 분리해서 구현
    - 임시 Data 추가

---
+++ src/data/ProjectContentData.tsx
@@ -0,0 +1,20 @@
+const projectContentData = {
+  reliable: {
+    tag: "Team Project",
+    title: "든붕이",
+    description: "든붕이 내용 설명",
+  },
+  sagopasam: {
+    tag: "Team Project",
+    title: "사고파삼",
+    description: "사고파삼 내용이 들어감",
+  },
+  portfolio: {
+    tag: "Solo Project",
+    title: "Portfolio",
+    description: "포트폴리오 내용 설명",
+  },
+  todo: { tag: "Solo Project", title: "Todo", description: "할 일 내용 설명" },
+};
+
+export default projectContentData;
