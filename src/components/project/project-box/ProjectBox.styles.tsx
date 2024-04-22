00a502895633c6f54f9d5a70715fcd5b613ccc43
zoodaa <jkj01230@gmail.com>
2024-04-23 00:30:21 +0900

    feat: Project 구현
    - ProjectImage 에 Image props 전달하도록 구현
    - ProjectContent 에 id props 전달하도록 구현

--- src/components/project/project-box/ProjectBox.styles.tsx
+++ src/components/project/project-box/ProjectBox.styles.tsx
@@ -48,42 +48,6 @@ export const Project = styled.div`
   }
 `;
 
-export const ImageBox = styled.div`
-  width: 210px;
-  height: 240px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-left: 10px;
-  border-radius: 10px;
-  box-shadow: 1px 2px 9px rgba(45, 45, 45, 0.3);
-
-  @media (max-width: ${smallViewport}) {
-    width: 150px;
-    height: 125px;
-    margin-left: 0;
-    margin-top: 10px;
-  }
-`;
-
-export const Image = styled.div`
-  width: 190px;
-  height: 220px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  .icon {
-    width: 100%;
-    height: 100%;
-  }
-
-  @media (max-width: ${smallViewport}) {
-    width: 140px;
-    height: 105px;
-  }
-`;
-
 export const ContentBox = styled.div`
   width: 52%;
   height: 90%;
--- src/components/project/project-box/ProjectBox.tsx
+++ src/components/project/project-box/ProjectBox.tsx
@@ -1,59 +1,37 @@
 import * as styles from "./ProjectBox.styles";
 import { useContext } from "react";
 import { MenuContext } from "../../../context/MenuContext";
+import ProjectImage from "../../project/project-box/project-image/ProjectImage";
 import { ReactComponent as ReliableKku } from "../../../assets/image/project/ReliableKku_Loge.svg";
 import { ReactComponent as Sagopasam } from "../../../assets/image/project/Sagopasam_Logo.svg";
 import { ReactComponent as Portfolio } from "../../../assets/image/project/Portfolio_Logo.svg";
 import { ReactComponent as Todo } from "../../../assets/image/project/Todo_Logo.svg";
-import ProjectTag from "../../common/tag/project-tag/ProjectTag";
+import ProjectContent from "./project-content/ProjectContent";
 
 const ProjectBox = () => {
   const { isMenuOpen } = useContext(MenuContext);
+  const menuBar = isMenuOpen ? "" : "none-menu";
 
   return (
-    <styles.ProjectBox className={isMenuOpen ? "" : "none-menu"}>
-      <styles.Project className={isMenuOpen ? "" : "none-menu"}>
-        <styles.ImageBox>
-          <styles.Image>
-            <ReliableKku className="icon" />
-          </styles.Image>
-        </styles.ImageBox>
-        <styles.ContentBox>
-          <ProjectTag text="Team Project" />
-        </styles.ContentBox>
+    <styles.ProjectBox className={menuBar}>
+      <styles.Project className={menuBar}>
+        <ProjectImage image={<ReliableKku className="icon" />} />
+        <ProjectContent id="reliable" />
       </styles.Project>
 
-      <styles.Project className={isMenuOpen ? "" : "none-menu"}>
-        <styles.ImageBox>
-          <styles.Image>
-            <Sagopasam className="icon" />
-          </styles.Image>
-        </styles.ImageBox>
-        <styles.ContentBox>
-          <ProjectTag text="Team Project" />
-        </styles.ContentBox>
+      <styles.Project className={menuBar}>
+        <ProjectImage image={<Sagopasam className="icon" />} />
+        <ProjectContent id="sagopasam" />
       </styles.Project>
 
-      <styles.Project className={isMenuOpen ? "" : "none-menu"}>
-        <styles.ImageBox>
-          <styles.Image>
-            <Portfolio className="icon" />
-          </styles.Image>
-        </styles.ImageBox>
-        <styles.ContentBox>
-          <ProjectTag text="Solo Project" />
-        </styles.ContentBox>
+      <styles.Project className={menuBar}>
+        <ProjectImage image={<Portfolio className="icon" />} />
+        <ProjectContent id="portfolio" />
       </styles.Project>
 
-      <styles.Project className={isMenuOpen ? "" : "none-menu"}>
-        <styles.ImageBox>
-          <styles.Image>
-            <Todo className="icon" />
-          </styles.Image>
-        </styles.ImageBox>
-        <styles.ContentBox>
-          <ProjectTag text="Solo Project" />
-        </styles.ContentBox>
+      <styles.Project className={menuBar}>
+        <ProjectImage image={<Todo className="icon" />} />
+        <ProjectContent id="todo" />
       </styles.Project>
     </styles.ProjectBox>
   );
