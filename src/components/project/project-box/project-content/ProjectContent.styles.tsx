07fe5d44e0a0d6fd5155a75e746c676e2587626f
zoodaa <jkj01230@gmail.com>
2024-04-23 18:08:11 +0900

    feat: Project 소개 구현
    - Project 소개 컴포넌트 구현

--- src/components/project/project-box/project-content/ProjectContent.styles.tsx
+++ src/components/project/project-box/project-content/ProjectContent.styles.tsx
@@ -3,15 +3,64 @@ import styled from "styled-components";
 const smallViewport = "62rem";
 
 export const ContentBox = styled.div`
+  position: relative;
   width: 52%;
   height: 90%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: bisque;
+  margin-right: 10px;
 
   @media (max-width: ${smallViewport}) {
     width: 90%;
-    height: 40%;
+    height: 20%;
+    margin-right: 0;
+
+    &.none-menu {
+      width: 90%;
+      height: 25%;
+      margin-right: 0;
+    }
+  }
+`;
+
+export const Title = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  color: #787878;
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 18px;
+  font-family:
+    Noto Sans KR,
+    sans-serif;
+
+  @media (max-width: ${smallViewport}) {
+    margin-top: 5px;
+    font-size: 16px;
+
+    &.none-menu {
+      margin-top: 10px;
+      font-size: 16px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  white-space: pre-line;
+  color: #787878;
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 28px;
+
+  @media (max-width: ${smallViewport}) {
+    margin-top: 10px;
+    font-size: 16px;
+
+    &.hide-description {
+      display: none;
+    }
   }
 `;
--- src/components/project/project-box/project-content/ProjectContent.tsx
+++ src/components/project/project-box/project-content/ProjectContent.tsx
@@ -1,23 +1,30 @@
 import * as styles from "./ProjectContent.styles";
 import ProjectTag from "../../../common/tag/project-tag/ProjectTag";
 import projectContentData from "../../../../data/ProjectContentData";
+import { useContext } from "react";
+import { MenuContext } from "../../../../context/MenuContext";
+import SkillTag from "../../../common/tag/skill-tag/SkillTag";
 
 interface ProjectContentProps {
   id: string;
 }
 
 const ProjectContent = ({ id }: ProjectContentProps) => {
-  const { tag, title, description } = projectContentData[id] || {
+  const { isMenuOpen } = useContext(MenuContext);
+  const menuBar = isMenuOpen ? "" : "none-menu";
+
+  const { tag, title, content } = projectContentData[id] || {
     tag: "",
     title: "",
-    description: "",
+    content: "",
   };
 
   return (
-    <styles.ContentBox>
+    <styles.ContentBox className={menuBar}>
       <ProjectTag text={tag} />
-      <div>{title}</div>
-      <div>{description}</div>
+      <styles.Title className={menuBar}>{title}</styles.Title>
+      <styles.Content className="hide-description">{content}</styles.Content>
+      <SkillTag />
     </styles.ContentBox>
   );
 };
