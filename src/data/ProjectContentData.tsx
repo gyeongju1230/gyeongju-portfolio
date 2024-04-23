d5c12fb9567786961219b40dee9d5ed1690cfcd5
zoodaa <jkj01230@gmail.com>
2024-04-23 18:08:17 +0900

    feat: Project 소개 data 구현

--- src/data/ProjectContentData.tsx
+++ src/data/ProjectContentData.tsx
@@ -2,19 +2,35 @@ const projectContentData = {
   reliable: {
     tag: "Team Project",
     title: "든붕이",
-    description: "든붕이 내용 설명",
+    content:
+      "든붕이는 \n" +
+      "건국대학교 글로컬캠퍼스 학우들이 사용하는 어플로, \n" +
+      "카페 '든든하쿠'의 사이렌오더 어플입니다.",
   },
   sagopasam: {
     tag: "Team Project",
     title: "사고파삼",
-    description: "사고파삼 내용이 들어감",
+    content:
+      "사고파삼은 \n" +
+      "단국대학교 학우들이 사용하는 어플로, \n" +
+      "전공서적 중고거래 플랫폼입니다.",
   },
   portfolio: {
     tag: "Solo Project",
-    title: "Portfolio",
-    description: "포트폴리오 내용 설명",
+    title: "GyeongJu Portfolio",
+    content:
+      "GyeongJu Portfolio는 \n" +
+      "개인 Portfolio 로 사용하기 위해 만든 \n" +
+      "반응형 웹사이드입니다.",
   },
-  todo: { tag: "Solo Project", title: "Todo", description: "할 일 내용 설명" },
+  todo: {
+    tag: "Solo Project",
+    title: "Todo Service",
+    content:
+      "Todo Service 는 \n" +
+      "사용자가 일정 관리를 쉽고 편리하게 할 수 있는 \n" +
+      "반응형 웹사이트입니다.",
+  },
 };
 
 export default projectContentData;
