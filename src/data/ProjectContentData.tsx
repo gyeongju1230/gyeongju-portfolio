fbdde7622ad3883e69c33fda9ba60a1072bc5229
zoodaa <jkj01230@gmail.com>
2024-04-24 18:04:07 +0900

    feat: ContentData 추가
    - 기존 임시 Data 삭제 후 픽스된 Data 추가

--- src/data/ProjectContentData.tsx
+++ src/data/ProjectContentData.tsx
@@ -3,15 +3,16 @@ const projectContentData = {
     tag: "Team Project",
     title: "든붕이",
     content:
-      "든붕이는 \n" +
-      "건국대학교 글로컬캠퍼스 학우들이 사용하는 어플로, \n" +
+      "'든붕이' 는 \n" +
+      "건국대학교 글로컬캠퍼스 학우들이 \n" +
+      "사용하는 어플로, \n" +
       "카페 '든든하쿠'의 사이렌오더 어플입니다.",
   },
   sagopasam: {
     tag: "Team Project",
     title: "사고파삼",
     content:
-      "사고파삼은 \n" +
+      "'사고파삼' 은 \n" +
       "단국대학교 학우들이 사용하는 어플로, \n" +
       "전공서적 중고거래 플랫폼입니다.",
   },
@@ -19,7 +20,7 @@ const projectContentData = {
     tag: "Solo Project",
     title: "GyeongJu Portfolio",
     content:
-      "GyeongJu Portfolio는 \n" +
+      "'GyeongJu Portfolio' 는 \n" +
       "개인 Portfolio 로 사용하기 위해 만든 \n" +
       "반응형 웹사이드입니다.",
   },
@@ -27,8 +28,9 @@ const projectContentData = {
     tag: "Solo Project",
     title: "Todo Service",
     content:
-      "Todo Service 는 \n" +
-      "사용자가 일정 관리를 쉽고 편리하게 할 수 있는 \n" +
+      "'Todo Service' 는 \n" +
+      "사용자가 일정 관리를 \n" +
+      "쉽고 편리하게 할 수 있도록 만든\n" +
       "반응형 웹사이트입니다.",
   },
 };
