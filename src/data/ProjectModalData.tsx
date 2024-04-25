b499012f59db2a217a17301541beed261f001515
zoodaa <jkj01230@gmail.com>
2024-04-25 15:56:59 +0900

    feat: Project Modal Data구현
    - Project Modal 에서 사용할 Data 파일 구현

---
+++ src/data/ProjectModalData.tsx
@@ -0,0 +1,28 @@
+const ProjectModalData = {
+  reliable: {
+    tag: "Team Project",
+    title: "든붕이",
+    contentTitle: "프로젝트 소개",
+    content: "든붕이는 어쩌고 저쩌고 소개다티비",
+  },
+  sagopasam: {
+    tag: "Team Project",
+    title: "사고파삼",
+    contentTitle: "프로젝트 소개",
+    content: "사고파삼은 어쩌고 저쩌고 소개다티비",
+  },
+  portfolio: {
+    tag: "Solo Project",
+    title: "GyeongJu Portfolio",
+    contentTitle: "프로젝트 소개",
+    content: "GyeongJu Portfolio 는 어쩌고 저쩌고 소개다티비",
+  },
+  todo: {
+    tag: "Solo Project",
+    title: "Todo Service",
+    contentTitle: "프로젝트 소개",
+    content: "Todo Service 는 어쩌고 저쩌고 소개다티비",
+  },
+};
+
+export default ProjectModalData;
