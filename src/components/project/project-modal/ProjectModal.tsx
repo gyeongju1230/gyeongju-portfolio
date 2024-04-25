7fe4b9acf546b371f07c8e3f0ef55a4474adb6e5
zoodaa <jkj01230@gmail.com>
2024-04-25 16:02:07 +0900

    feat: Project Modal 구현
    - Project id 를 받아서 해당하는 id 의 tag, contentTitle, content 노출되도록 구현

--- src/components/project/project-modal/ProjectModal.tsx
+++ src/components/project/project-modal/ProjectModal.tsx
@@ -1,10 +1,26 @@
 import * as styles from "./ProjectModal.styles";
+import ProjectTag from "../../common/tag/project-tag/ProjectTag";
+import projectModalData from "../../../data/ProjectModalData";
 
-const ProjectModal = () => {
+interface ProjectModalProps {
+  id: string;
+}
+
+const ProjectModal = ({ id }: ProjectModalProps) => {
+  const { tag, title, contentTitle, content } = projectModalData[id] || {
+    tag: "",
+    title: "",
+    contentTitle: "",
+    content: "",
+  };
+
   return (
     <styles.ModalWrapper>
       <styles.ModalContainer>
-        <styles.ModalBox>sdsd</styles.ModalBox>
+        <styles.ModalBox>
+          <ProjectTag text={tag} />
+          {title}
+        </styles.ModalBox>
       </styles.ModalContainer>
     </styles.ModalWrapper>
   );
