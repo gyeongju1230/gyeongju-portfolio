8ce5fffb4aca59b13b2ba31122e942d6357d38f2
zoodaa <jkj01230@gmail.com>
2024-05-12 00:35:01 +0900

    feat: About page Content/Link 구현
    - 픽스된 Content 삽입
    - Github/Notion 외부 링크 연결 구현

--- src/components/about/about-content/AboutContent.tsx
+++ src/components/about/about-content/AboutContent.tsx
@@ -8,6 +8,8 @@ import { ReactComponent as Notion } from "../../../assets/icon/skill/Notion.svg"
 const AboutContent = () => {
   const { isMenuOpen } = useContext(MenuContext);
   const menuBar = isMenuOpen ? "" : "none-menu";
+  const githubUrl = "https://github.com/gyeongju1230";
+  const notionUrl = "https://bit.ly/4cUYb0S";
 
   return (
     <styles.AboutBox className={menuBar}>
@@ -26,11 +28,13 @@ const AboutContent = () => {
         />
       </styles.AboutTitle>
       <styles.AboutContent>
-        ✔️ 배움을 즐기고 새로운 지식을 얻는 것을 좋아하는 열정파입니다.
+        ✔️ 끊임없는 성장을 추구하며, 배움을 즐깁니다.
         <br />
-        ✔️ 스스로의 성장을 위한 의지와 집념이 강합니다.
+        ✔️ 새로운 도전에 열려 있는 성격으로 항상 더 나은 개발자로 거듭나기 위해
+        노력합니다.
         <br />
-        ✔️ 스스로의 성장을 위한 의지와 집념이 강합니다.
+        ✔️ 저는 강한 책임감을 가지고 프로젝트에 최선을 다하며, 팀원들과 협력하여
+        목표 달성을 위해 노력합니다.
         <br />
       </styles.AboutContent>
       <styles.AboutSkillBox>
@@ -39,11 +43,19 @@ const AboutContent = () => {
         <styles.AboutSkills>React.js</styles.AboutSkills>
       </styles.AboutSkillBox>
       <styles.AboutLinkBox>
-        <styles.AboutLink>
+        <styles.AboutLink
+          onClick={() => {
+            window.open(githubUrl);
+          }}
+        >
           <Github className="icon" />
           https://github.com/gyeongju1230
         </styles.AboutLink>
-        <styles.AboutLink>
+        <styles.AboutLink
+          onClick={() => {
+            window.open(notionUrl);
+          }}
+        >
           <Notion className="icon" />
           https://bit.ly/4cUYb0S
         </styles.AboutLink>
