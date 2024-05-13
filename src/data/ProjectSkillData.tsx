62895b090af8a94e3c6f9737a159526959d1a5b5
zoodaa <jkj01230@gmail.com>
2024-05-13 21:25:46 +0900

    feat: Project Skills Data 추가

---
+++ src/data/ProjectSkillData.tsx
@@ -0,0 +1,29 @@
+const projectSkillData = {
+  portfolio: {
+    skills: [
+      "React",
+      "TypeScript",
+      "Aws Amplify",
+      "Styled-Component",
+      "Framer Motion",
+      "Figma",
+    ],
+  },
+  todo: {
+    skills: ["React", "TypeScript", "Aws Amplify", "Styled-Component", "Figma"],
+  },
+  sagopasam: {
+    skills: [
+      "React Native",
+      "Short Polling",
+      "Axios",
+      "Recoil",
+      "Styled-Component",
+    ],
+  },
+  deunbung: {
+    skills: ["React Native", "TypeScript", "SSE", "Axios", "Styled-Component"],
+  },
+};
+
+export default projectSkillData;
