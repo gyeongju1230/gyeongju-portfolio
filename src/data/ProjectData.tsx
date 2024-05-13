9287c658866105c9e6d3af82b20edd432508dc31
zoodaa <jkj01230@gmail.com>
2024-05-13 21:27:46 +0900

    feat: Project Data 추가
    - 픽스된 Project Data 추가
    - 강조할 text 에 strong 태그 추가

--- src/data/ProjectData.tsx
+++ src/data/ProjectData.tsx
@@ -3,35 +3,38 @@ const projectData = {
     tag: "Solo Project",
     title: "GyeongJu Portfolio",
     content:
-      "'GyeongJu Portfolio' 는 \n" +
-      "개인 Portfolio 로 사용하기 위해 만든 \n" +
-      "반응형 웹사이드입니다.",
+      "<strong>'GyeongJu Portfolio'</strong> 는 \n" +
+      "개인 Portfolio 용도로 제작한 \n" +
+      "반응형 웹사이트입니다. \n" +
+      "현재 이 웹사이트에 해당합니다.",
   },
   todo: {
     tag: "Solo Project",
     title: "Todo Service",
     content:
-      "'Todo Service' 는 \n" +
-      "사용자가 일정 관리를 \n" +
-      "쉽고 편리하게 할 수 있도록 만든\n" +
+      "<strong>'Todo Service'</strong> 는 \n" +
+      "직접 여러 Todo 어플을 사용해보고 느낀\n" +
+      "불편함을 개선하여 일정 관리를\n" +
+      "더욱 쉽고 편리하게 할 수 있도록 제작한\n" +
       "반응형 웹사이트입니다.",
   },
   sagopasam: {
     tag: "Team Project",
     title: "사고파삼",
     content:
-      "'사고파삼' 은 \n" +
-      "단국대학교 학우들이 사용하는 어플로, \n" +
-      "전공서적 중고거래 플랫폼입니다.",
+      "<strong>'사고파삼'</strong> 은 \n" +
+      "단국대학교 죽전캠퍼스 학우들과\n" +
+      "협업하여 제작한\n" +
+      "전공서적 중고거래 애플리케이션입니다.\n",
   },
   deunbung: {
     tag: "Team Project",
     title: "든붕이",
     content:
-      "'든붕이' 는 \n" +
-      "건국대학교 글로컬캠퍼스 학우들이 \n" +
-      "사용하는 어플로, \n" +
-      "카페 '든든하쿠'의 사이렌오더 어플입니다.",
+      "<strong>'든붕이'</strong> 는 \n" +
+      "건국대학교 글로컬캠퍼스 창업동아리 카페 \n" +
+      "'든든하쿠'와 협업하여 제작한\n" +
+      "붕어빵 미리 주문 애플리케이션입니다.",
   },
 };
 
