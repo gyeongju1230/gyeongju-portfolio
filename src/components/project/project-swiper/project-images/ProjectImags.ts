e22c08058c4f88be2aba72673e19e4da3ac8fa58
zoodaa <jkj01230@gmail.com>
2024-05-23 23:52:43 +0900

    feat: Project Image 구현
    - Project Modal 에서 보여질 Image 파일 분리해서 구현

---
+++ src/components/project/project-swiper/project-images/ProjectImags.ts
@@ -0,0 +1,63 @@
+import AppStoreImagePortfolio1 from '@assets/images/project/portfolio/Portfolio_AppStoreImage1.svg';
+import AppStoreImagePortfolio2 from '@assets/images/project/portfolio/Portfolio_AppStoreImage2.svg';
+import AppStoreImagePortfolio3 from '@assets/images/project/portfolio/Portfolio_AppStoreImage3.svg';
+import AppStoreImagePortfolio4 from '@assets/images/project/portfolio/Portfolio_AppStoreImage4.svg';
+import AppStoreImagePortfolio5 from '@assets/images/project/portfolio/Portfolio_AppStoreImage5.svg';
+import AppStoreImageTodo1 from '@assets/images/project/todo/Todo_AppStoreImage1.svg';
+import AppStoreImageTodo2 from '@assets/images/project/todo/Todo_AppStoreImage2.svg';
+import AppStoreImageTodo3 from '@assets/images/project/todo/Todo_AppStoreImage3.svg';
+import AppStoreImageTodo4 from '@assets/images/project/todo/Todo_AppStoreImage4.svg';
+import AppStoreImageTodo5 from '@assets/images/project/todo/Todo_AppStoreImage5.svg';
+import AppStoreImageSagopasam1 from '@assets/images/project/sagopasam/Sagopasam_AppStoreImage1.svg';
+import AppStoreImageSagopasam2 from '@assets/images/project/sagopasam/Sagopasam_AppStoreImage2.svg';
+import AppStoreImageSagopasam3 from '@assets/images/project/sagopasam/Sagopasam_AppStoreImage3.svg';
+import AppStoreImageSagopasam4 from '@assets/images/project/sagopasam/Sagopasam_AppStoreImage4.svg';
+import AppStoreImageSagopasam5 from '@assets/images/project/sagopasam/Sagopasam_AppStoreImage5.svg';
+import AppStoreImageSagopasam6 from '@assets/images/project/sagopasam/Sagopasam_AppStoreImage6.svg';
+import AppStoreImageDeunbung1 from '@assets/images/project/deunbung/Deunbung_AppStoreImage1.svg';
+import AppStoreImageDeunbung2 from '@assets/images/project/deunbung/Deunbung_AppStoreImage2.svg';
+import AppStoreImageDeunbung3 from '@assets/images/project/deunbung/Deunbung_AppStoreImage3.svg';
+import AppStoreImageDeunbung4 from '@assets/images/project/deunbung/Deunbung_AppStoreImage4.svg';
+import AppStoreImageDeunbung5 from '@assets/images/project/deunbung/Deunbung_AppStoreImage5.svg';
+import AppStoreImageDeunbung6 from '@assets/images/project/deunbung/Deunbung_AppStoreImage6.svg';
+import AppStoreImageDeunbung7 from '@assets/images/project/deunbung/Deunbung_AppStoreImage7.svg';
+
+interface Images {
+  [key: string]: string[];
+}
+
+const images: Images = {
+  portfolio: [
+    AppStoreImagePortfolio1,
+    AppStoreImagePortfolio2,
+    AppStoreImagePortfolio3,
+    AppStoreImagePortfolio4,
+    AppStoreImagePortfolio5,
+  ],
+  todo: [
+    AppStoreImageTodo1,
+    AppStoreImageTodo2,
+    AppStoreImageTodo3,
+    AppStoreImageTodo4,
+    AppStoreImageTodo5,
+  ],
+  sagopasam: [
+    AppStoreImageSagopasam1,
+    AppStoreImageSagopasam2,
+    AppStoreImageSagopasam3,
+    AppStoreImageSagopasam4,
+    AppStoreImageSagopasam5,
+    AppStoreImageSagopasam6,
+  ],
+  deunbung: [
+    AppStoreImageDeunbung1,
+    AppStoreImageDeunbung2,
+    AppStoreImageDeunbung3,
+    AppStoreImageDeunbung4,
+    AppStoreImageDeunbung5,
+    AppStoreImageDeunbung6,
+    AppStoreImageDeunbung7,
+  ],
+};
+
+export default images;
