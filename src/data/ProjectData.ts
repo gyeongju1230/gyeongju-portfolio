148e93165fb7500a3205f07998b2dee50046fd8e
zoodaa <jkj01230@gmail.com>
2024-05-24 17:13:23 +0900

    feat: Project Data 변경

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -1,40 +1,45 @@
 const projectData = {
   portfolio: {
-    tag: "Solo Project",
-    title: "GyeongJu Portfolio",
+    tag: 'Solo Project',
+    title: 'GyeongJu Portfolio',
     content:
-      "<strong>'GyeongJu Portfolio'</strong> 는 \n" +
-      "개인 Portfolio 용도로 제작한 \n" +
-      "반응형 웹사이트입니다. \n" +
-      "현재 이 웹사이트에 해당합니다.",
+      "'GyeongJu Portfolio' 는 \n" +
+      ' 개인 Portfolio 용도로 제작한 반응형 웹사이트입니다. \n' +
+      '현재 이 웹사이트에 해당합니다.',
+    demo: 'demo link',
+    github: 'github link',
   },
   todo: {
-    tag: "Solo Project",
-    title: "Todo Service",
+    tag: 'Solo Project',
+    title: 'Todo Service',
     content:
-      "<strong>'Todo Service'</strong> 는 \n" +
-      "직접 여러 Todo 어플을 사용해보고 느낀\n" +
-      "불편함을 개선하여 일정 관리를\n" +
-      "더욱 쉽고 편리하게 할 수 있도록 제작한\n" +
-      "반응형 웹사이트입니다.",
+      "'Todo Service' 는 \n" +
+      '직접 여러 Todo 어플을 사용해보고 느낀 불편함을 개선하여 \n' +
+      '일정 관리를 더욱 쉽고 편리하게 할 수 있도록 제작한\n' +
+      '반응형 웹사이트입니다.',
+    demo: 'https://gyeongju-todo-service.com',
+    github: 'https://github.com/gyeongju1230/todo-service',
   },
   sagopasam: {
-    tag: "Team Project",
-    title: "사고파삼",
+    tag: 'Team Project',
+    title: '사고파삼',
     content:
-      "<strong>'사고파삼'</strong> 은 \n" +
-      "단국대학교 죽전캠퍼스 학우들과\n" +
-      "협업하여 제작한\n" +
-      "전공서적 중고거래 애플리케이션입니다.\n",
+      "'사고파삼' 은 \n" +
+      '단국대학교 죽전캠퍼스 학우들과 협업하여 제작한 \n' +
+      '전공서적 중고거래 애플리케이션입니다.\n',
+    demo: 'demo link',
+    github: 'github link',
   },
   deunbung: {
-    tag: "Team Project",
-    title: "든붕이",
+    tag: 'Team Project',
+    title: '든붕이',
     content:
-      "<strong>'든붕이'</strong> 는 \n" +
-      "건국대학교 글로컬캠퍼스 창업동아리 카페 \n" +
-      "'든든하쿠'와 협업하여 제작한\n" +
-      "붕어빵 미리 주문 애플리케이션입니다.",
+      "'든붕이' 는 \n" +
+      '건국대학교 글로컬캠퍼스 창업동아리 카페 \n' +
+      "'든든하쿠' 와 협업하여 제작한\n" +
+      '붕어빵 미리 주문 애플리케이션입니다.',
+    demo: 'demo link',
+    github: 'github link',
   },
 };
 
