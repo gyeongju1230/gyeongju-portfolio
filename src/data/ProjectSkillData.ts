c08910bd4f1ae12b0b8480b6a5f9b553510af81d
zoodaa <jkj01230@gmail.com>
2024-06-03 17:26:52 +0900

    feat: Skill 추가

--- src/data/ProjectSkillData.ts
+++ src/data/ProjectSkillData.ts
@@ -1,28 +1,31 @@
 const projectSkillData = {
   portfolio: {
     skills: [
-      "React",
-      "TypeScript",
-      "Aws Amplify",
-      "Framer Motion",
-      "Styled-Component",
-      "Figma",
+      'React',
+      'TypeScript',
+      'Aws Amplify',
+      'Framer Motion',
+      'Lottie',
+      'Swiper',
+      'Styled-Component',
+      'Figma',
     ],
   },
   todo: {
-    skills: ["React", "Aws Amplify", "CSS", "Styled-Component", "Figma"],
+    skills: ['React', 'Aws Amplify', 'CSS', 'Styled-Component', 'Figma'],
   },
   sagopasam: {
     skills: [
-      "React Native",
-      "Short Polling",
-      "Axios",
-      "Recoil",
-      "Styled-Component",
+      'React Native',
+      'Short Polling',
+      'Axios',
+      'Recoil',
+      'Swiper',
+      'Styled-Component',
     ],
   },
   deunbung: {
-    skills: ["React Native", "TypeScript", "SSE", "Axios", "Styled-Component"],
+    skills: ['React Native', 'TypeScript', 'SSE', 'Axios', 'Styled-Component'],
   },
 };
 
