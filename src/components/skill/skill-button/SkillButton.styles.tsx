a0b58241c6a62152379f41b2aabc820b0ea2bf6a
zoodaa <jkj01230@gmail.com>
2024-06-04 00:08:00 +0900

    feat: Lottie 구현
    - Skill Button 을 직관적으로 보이기위해 Arrow Lottie 와 Text 추가

--- src/components/skill/skill-button/SkillButton.styles.tsx
+++ src/components/skill/skill-button/SkillButton.styles.tsx
@@ -1,15 +1,16 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-const smallViewport = "62rem";
+const smallViewport = '62rem';
 
 export const Container = styled.div`
   width: 40%;
   margin-top: 90px;
+  gap: 20px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 
   @media (max-width: ${smallViewport}) {
     width: 100%;
@@ -18,6 +19,10 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: center;
 
+    .arrow {
+      display: none;
+    }
+
     &.none-menu {
       width: 100%;
       height: 30%;
@@ -25,6 +30,7 @@ export const Container = styled.div`
     }
   }
 `;
+
 export const Box = styled.div`
   position: relative;
   width: 340px;
@@ -66,23 +72,22 @@ export const ButtonBox = styled(motion.div)`
   justify-content: center;
   border-radius: 50%;
   background-color: #fbe5d8;
+  box-shadow: 1px 2px 9px rgba(45, 45, 45, 0.3);
 
   color: #a07d5a;
-  font-size: 30px;
+  font-size: 22px;
   font-weight: bold;
-  font-family:
-    Noto Sans KR,
-    sans-serif;
+  font-family: GmarketSans, sans-serif;
 
   @media (max-width: ${smallViewport}) {
     width: 55px;
     height: 55px;
-    font-size: 20px;
+    font-size: 12px;
 
     &.none-menu {
       width: 64px;
       height: 64px;
-      font-size: 20px;
+      font-size: 14px;
     }
   }
 `;
@@ -108,4 +113,30 @@ export const Button = styled(motion.button)`
       }
     }
   }
+`;
+
+export const ArrowBox = styled.div`
+  width: 340px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+
+  .lottie {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const LottieBox = styled.div`
+  width: 46px;
+  height: 30px;
+`;
+
+export const ArrowContent = styled.div`
+  color: #a07d5a;
+  font-size: 16px;
+  font-weight: normal;
+  line-height: 45px;
+  font-family: GmarketSans, sans-serif;
 `;
--- src/components/skill/skill-button/SkillButton.tsx
+++ src/components/skill/skill-button/SkillButton.tsx
@@ -1,5 +1,7 @@
-import * as styles from "./SkillButton.styles";
-import { Fragment } from "react";
+import * as styles from './SkillButton.styles';
+import { Fragment } from 'react';
+import Lottie from 'lottie-react';
+import ArrowLottie from '@assets/lottie/Lottie_arrow.json';
 
 interface SkillButtonProps {
   menuBar: string;
@@ -20,7 +22,7 @@ const SkillButton = ({
               <styles.SelectedButton>
                 <styles.Button
                   onClick={() => handleButtonClick(index)}
-                  className={selectedButtonIndex === index ? "selected" : ""}
+                  className={selectedButtonIndex === index ? 'selected' : ''}
                   whileHover={{ scale: 1.1 }}
                   whileTap={{ scale: 0.9 }}
                 >
@@ -28,31 +30,37 @@ const SkillButton = ({
                     initial={{ opacity: 0, scale: 0 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
-                      type: "spring",
+                      type: 'spring',
                       stiffness: 260,
                       damping: 20,
                     }}
                     className={menuBar}
                   >
-                    {index === 0 ? "FE" : index === 1 ? "BE" : "TOOL"}
+                    {index === 0 ? 'FE' : index === 1 ? 'BE' : 'TOOL'}
                   </styles.ButtonBox>
                 </styles.Button>
               </styles.SelectedButton>
             ) : (
               <styles.Button
                 onClick={() => handleButtonClick(index)}
-                className={selectedButtonIndex === index ? "selected" : ""}
+                className={selectedButtonIndex === index ? 'selected' : ''}
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
               >
                 <styles.ButtonBox className={menuBar}>
-                  {index === 0 ? "FE" : index === 1 ? "BE" : "TOOL"}
+                  {index === 0 ? 'FE' : index === 1 ? 'BE' : 'TOOL'}
                 </styles.ButtonBox>
               </styles.Button>
             )}
           </Fragment>
         ))}
       </styles.Box>
+      <styles.ArrowBox className="arrow">
+        <styles.ArrowContent>버튼을 눌러주세요!</styles.ArrowContent>
+        <styles.LottieBox>
+          <Lottie animationData={ArrowLottie} className="lottie" />
+        </styles.LottieBox>
+      </styles.ArrowBox>
     </styles.Container>
   );
 };
