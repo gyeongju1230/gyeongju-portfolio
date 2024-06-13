dd734ef26892fbdea98f3fc45d1ac0a62e879d77
zoodaa <jkj01230@gmail.com>
2024-06-14 00:17:33 +0900

    refactor: styles 수정

--- src/components/pagination/Pagination.styles.tsx
+++ src/components/pagination/Pagination.styles.tsx
@@ -6,6 +6,7 @@ export const PaginationContainer = styled.div`
   position: fixed;
   display: flex;
   flex-direction: column;
+  align-items: center;
   top: 172px;
   right: 35px;
   gap: 12px;
@@ -23,7 +24,7 @@ export const PaginationContainer = styled.div`
 `;
 
 export const PageItem = styled.div<{ isActive: boolean }>`
-  width: 16px;
+  width: ${props => (props.isActive ? '20px' : '16px')};
   height: 2px;
   margin: 5px 0;
   border-radius: 10px;
@@ -31,7 +32,7 @@ export const PageItem = styled.div<{ isActive: boolean }>`
     props.isActive ? '#d8b28c' : 'rgb(227, 226, 224)'};
 
   @media (max-width: ${smallViewport}) {
-    width: 12px;
+    width: ${props => (props.isActive ? '16px' : '12px')};
     height: 2px;
     margin: 2.5px 0;
     border: 5px;
